7d9c351de5ccb9ff7ffb466281238469a4464f6d
aksharasisodiya833-tech <aksharasisodiya833@gmail.com>
2026-04-28 23:59:11 +0530

    Lambda Expression

---
+++ src/InvoiceObjectCreation/Invoice.java
@@ -0,0 +1,14 @@
+package InvoiceObjectCreation;
+
+public class Invoice {
+        String transactionId;
+
+        Invoice(String transactionId) {
+            this.transactionId = transactionId;
+        }
+
+        public String toString() {
+            return "Invoice for Transaction: " + transactionId;
+        }
+    }
+
---
+++ src/NotificationFiltering/Alert.java
@@ -0,0 +1,17 @@
+package NotificationFiltering;
+
+public class Alert {
+
+        String type;
+        String message;
+
+        Alert(String type, String message) {
+            this.type = type;
+            this.message = message;
+        }
+
+        public String toString() {
+            return type + " : " + message;
+        }
+    }
+
